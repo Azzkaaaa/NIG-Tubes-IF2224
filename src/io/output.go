0528b0df2dba4d4b5e4c87b6c70ca144ad932510
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-10-15 17:52:11 +0700

    feat: add output

--- src/io/output.go
+++ src/io/output.go
@@ -1,1 +1,37 @@
 package io
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/Azzkaaaa/NIG-Tubes-IF2224/src/datatype"
+)
+
+func PrintTokens(tokens []datatype.Token) {
+	for _, t := range tokens {
+		fmt.Printf("%s(%s) @ %d:%d\n", t.Type.String(), t.Lexeme, t.Line, t.Col)
+	}
+}
+
+func PrintErrors(errs []error) {
+	for _, e := range errs {
+		fmt.Fprintln(os.Stderr, e)
+	}
+}
+
+func WriteTokensToFile(path string, tokens []datatype.Token) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	for _, t := range tokens {
+		if _, err := fmt.Fprintf(w, "%s(%s) @ %d:%d\n", t.Type.String(), t.Lexeme, t.Line, t.Col); err != nil {
+			return err
+		}
+	}
+	return w.Flush()
+}
