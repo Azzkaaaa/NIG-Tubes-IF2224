16cb2bae721363633aefbc48c081284b211e7ff0
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-10-15 11:10:56 +0700

    feat: add datatype for token

--- src/datatype/datatype.go
+++ src/datatype/datatype.go
@@ -1,1 +1,46 @@
 package datatype
+
+type TokenType int
+
+const (
+	KEYWORD TokenType = iota
+	IDENTIFIER
+	ARITHMETIC_OPERATOR
+	RELATIONAL_OPERATOR
+	LOGICAL_OPERATOR
+	ASSIGN_OPERATOR
+	NUMBER
+	CHAR_LITERAL
+	STRING_LITERAL
+	SEMICOLON
+	COMMA
+	COLON
+	DOT
+	LPARENTHESIS
+	RPARENTHESIS
+	LBRACKET
+	RBRACKET
+	RANGE_OPERATOR
+	COMMENT_START
+	COMMENT_END
+)
+
+func (t TokenType) String() string {
+	names := [...]string{
+		"KEYWORD", "IDENTIFIER", "ARITHMETIC_OPERATOR", "RELATIONAL_OPERATOR", "LOGICAL_OPERATOR",
+		"ASSIGN_OPERATOR", "NUMBER", "CHAR_LITERAL", "STRING_LITERAL", "SEMICOLON", "COMMA", "COLON",
+		"DOT", "LPARENTHESIS", "RPARENTHESIS", "LBRACKET", "RBRACKET", "RANGE_OPERATOR",
+		"COMMENT_START", "COMMENT_END",
+	}
+	if int(t) < 0 || int(t) >= len(names) {
+		return "UNKNOWN"
+	}
+	return names[t]
+}
+
+var Keywords = map[string]struct{}{
+	"program": {}, "var": {}, "begin": {}, "end": {}, "if": {}, "then": {}, "else": {},
+	"while": {}, "do": {}, "for": {}, "to": {}, "downto": {},
+	"integer": {}, "real": {}, "boolean": {}, "char": {},
+	"array": {}, "of": {}, "procedure": {}, "function": {}, "const": {}, "type": {},
+}
