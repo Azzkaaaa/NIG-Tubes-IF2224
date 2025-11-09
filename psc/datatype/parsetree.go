aa7d0aeecc8904ebe57c6a8886ebbe73cc637d83
L4mbads <rizacal.mamen@gmail.com>
2025-11-10 06:13:53 +0700

    feat: add pipes to tree drawing

--- psc/datatype/parsetree.go
+++ psc/datatype/parsetree.go
@@ -88,25 +88,38 @@ func (t NodeType) String() string {
 
 func (t ParseTree) String() string {
 	var sb strings.Builder
-	t.writeString(&sb, 0)
+	t.writeString(&sb, "", true, true)
 	return sb.String()
 }
 
-func (t ParseTree) writeString(sb *strings.Builder, indent int) {
-	prefix := strings.Repeat("  ", indent)
-	sb.WriteString(prefix)
+func (t ParseTree) writeString(sb *strings.Builder, prefix string, isLast bool, isRoot bool) {
+	if prefix != "" {
+		if isLast {
+			sb.WriteString(prefix + "└-")
+		} else {
+			sb.WriteString(prefix + "├-")
+		}
+	}
+
 	if t.RootType != TOKEN_NODE {
 		sb.WriteString(t.RootType.String())
 	}
-
 	if t.TokenValue != nil {
 		sb.WriteString(t.TokenValue.Type.String())
 		sb.WriteString(fmt.Sprintf(" (%s)", t.TokenValue.Lexeme))
 	}
-
 	sb.WriteString("\n")
 
-	for _, child := range t.Children {
-		child.writeString(sb, indent+1)
+	childPrefix := prefix
+	if isRoot || prefix != "" {
+		if isLast {
+			childPrefix += "  "
+		} else {
+			childPrefix += "│ "
+		}
+	}
+
+	for i, child := range t.Children {
+		child.writeString(sb, childPrefix, i == len(t.Children)-1, false)
 	}
 }
